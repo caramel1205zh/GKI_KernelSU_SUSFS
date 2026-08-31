9bc173e1754dc1b710ecd0568bcc9829f6e05aea
caramel1205zh <forgezhzh@outlook.com>
2026-08-31 19:20:51 +0800

    Change the 6.1 workflow to manet build

--- scripts/ksu_post_setup.sh
+++ scripts/ksu_post_setup.sh
@@ -12,7 +12,7 @@ declare -A PATTERNS
 PATTERNS[resukisu]=$(echo 'UkVQT19OQU1FIDo9IFJlU3VraVNV' | base64 -d | base64)
 PATTERNS[sukisu_branch]=$(echo 'QCQoY2FsbCBnaXRfYnJhbmNoKQ==' | base64 -d | base64)
 PATTERNS[marker]=$(echo 'UmVTdWtpU1U=' | base64 -d | base64)
-BRAND=$(echo 'UkVETUk=' | base64 -d)
+BRAND=$(TZ='Asia/Shanghai' date '+%Y%m%d')
 
 apply_resukisu() {
   local kbuild="$KSU_DIR/kernel/Kbuild"
